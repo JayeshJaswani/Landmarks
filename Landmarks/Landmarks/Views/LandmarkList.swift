6fcc47cdb54a7fb002c801317ccc58c7965d8fe0
Jayesh Jaswani <62129972+JayeshJaswani@users.noreply.github.com>
2022-11-04 18:02:53 +0400

    Add files via upload

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    @State private var showingProfile = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+            .toolbar {
+                            Button {
+                                showingProfile.toggle()
+                            } label: {
+                                Label("User Profile", systemImage: "person.crop.circle")
+                            }
+                        }
+                        .sheet(isPresented: $showingProfile) {
+                            ProfileHost()
+                                .environmentObject(modelData)
+                        }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
